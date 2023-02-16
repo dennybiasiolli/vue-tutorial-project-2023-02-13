e89bab400d4c055b2abbe3608a327b21f0efc128
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2023-02-16 12:33:20 +0100

    day 4, step 1

--- src/components/TodoUtility.vue
+++ src/components/TodoUtility.vue
@@ -1,11 +1,15 @@
 <script setup>
 import { storeToRefs } from 'pinia'
-import { onMounted } from 'vue'
+import { ref, watch, onMounted } from 'vue'
 import { useTodoStore } from '@/stores/todo'
 import TodoList from './TodoList.vue'
 
 const todoStore = useTodoStore()
 const { items, completedItems, todoItems } = storeToRefs(todoStore)
+const { addTodo, switchTodo } = todoStore
+
+const formValid = ref(false)
+const itemText = ref('')
 
 onMounted(() => {
   items.value = [
@@ -18,12 +22,30 @@ onMounted(() => {
 })
 
 function handleSwitchTodo({ id, completed }) {
-  todoStore.switchTodo(id, completed)
+  switchTodo(id, completed)
 }
+
+function validateForm() {
+  formValid.value = !!itemText.value.trim();
+}
+
+function handleSubmit() {
+  addTodo(itemText.value)
+  itemText.value = ''
+}
+
+watch(itemText, () => {
+  validateForm()
+})
 </script>
 
 <template>
   <h2>Todo Utility</h2>
+
+  <form @submit="handleSubmit">
+    <input type="text" placeholder="To-do item" v-model="itemText" />
+    <button type="submit" :disabled="!formValid">Add</button>
+  </form>
 
   <TodoList
     v-if="todoItems.length > 0"
--- src/stores/todo.js
+++ src/stores/todo.js
@@ -7,6 +7,9 @@ export const useTodoStore = defineStore('todo', {
     todoItems: (state) => state.items.filter((item) => !item.completed),
   },
   actions: {
+    addTodo(text) {
+      console.log('addTodo', text)
+    },
     switchTodo(id, completed) {
       const item = this.items.find((item) => item.id === id)
       item.completed = completed
